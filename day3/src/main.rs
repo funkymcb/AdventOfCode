f035811c1bf556f9a195f0ec3ef75f5f0f0109b1
Yannick Peter <yannick.peter@empiriecom.com>
2023-12-02 21:21:56 +0100

    prepare input reader

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::process::exit;
+
+const INPUT_PATH: &str = "input";
+
 fn main() {
-    println!("ready for day 3!");
+    let file = File::open(INPUT_PATH).expect("could not read file");
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        if line.is_ok() {
+
+        } else {
+            println!("could not read line");
+            exit(1)
+        }
+    }
 }

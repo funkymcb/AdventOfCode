429cd64ed3a9dd16055f7c85be038a8538364e17
Yannick Peter <yannick.peter@empiriecom.com>
2023-12-02 11:43:23 +0100

    refactor functions to methods

--- day2/src/game.rs
+++ day2/src/game.rs
@@ -1,4 +1,8 @@
 use core::fmt;
+use std::process::exit;
+use regex::Regex;
+
+const GAME_REGEX: &str = r"(Game (?P<id>\d+))";
 
 #[derive(Debug)]
 pub struct Game {
@@ -8,9 +12,34 @@ pub struct Game {
     pub blue_cubes_count: u8,
 }
 
+impl Game {
+    pub fn new() -> Game {
+        Game {
+            id: 0,
+            red_cube_count: 0,
+            green_cubes_count: 0,
+            blue_cubes_count: 0
+        }
+    }
+
+    pub fn analyze(&mut self, line: &str) {
+       let re = Regex::new(GAME_REGEX).unwrap();
+       let Some(caps) = re.captures(line) else {
+           println!("no line matched regex");
+           exit(1)
+       };
+
+       self.id = caps["id"].parse::<u8>().unwrap();
+    }
+}
+
 impl fmt::Display for Game {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "game id: {}\n red cubes: {}\n green cubes: {}\n blue cubes: {}",
-               self.id, self.red_cube_count, self.green_cubes_count, self.blue_cubes_count)
+           self.id,
+           self.red_cube_count,
+           self.green_cubes_count,
+           self.blue_cubes_count
+        )
     }
 }
--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,15 +1,11 @@
 mod game;
-mod error;
 
-use crate::error::DynError;
 use crate::game::Game;
-use regex::Regex;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::process::exit;
 
 const INPUT_PATH: &str = "input";
-const GAME_REGEX: &str = r"(Game (?P<id>\d+))";
 
 fn main() {
     let file = File::open(INPUT_PATH)
@@ -21,25 +17,12 @@ fn main() {
             let line: String = line.unwrap();
 
             // star1
-            let game = analyze_game(line.as_str());
-            println!("{}", game.unwrap())
+            let mut game = Game::new();
+            game.analyze(line.as_str());
+            println!("{}", &game);
         } else {
             println!("could not read line");
             exit(1)
         }
     }
-}
-
-fn analyze_game(line: &str) -> Result<Game, DynError> {
-   let re = Regex::new(GAME_REGEX).unwrap();
-   let Some(caps) = re.captures(line) else {
-       return Err(DynError::new("no line matched regex"));
-   };
-
-   Ok(Game {
-       id: caps["id"].parse::<u8>().unwrap(),
-       red_cube_count: 1,
-       green_cubes_count: 2,
-       blue_cubes_count: 3
-   })
 }
